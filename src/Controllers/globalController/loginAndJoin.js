897de8bd4b79fb998749706f821a5eda1476567a
coffeed-cat <gwcattower@gmail.com>
2021-07-26 20:45:30 +0900

    [ globalController/loginAndJoin.js ] 로그인 & 회원가입 함수 작성 완료

--- src/Controllers/globalController/loginAndJoin.js
+++ src/Controllers/globalController/loginAndJoin.js
@@ -1,21 +1,53 @@
-export const login = (req, res) => {
+import jwt from "jsonwebtoken";
+import SHA256 from "crypto-js/sha256";
+import User from "../../model/user";
+import SECRET_KEY from "../../config/secretKey";
+
+export const login = async (req, res) => {
   try {
+    const { id, password } = req.body;
+
+    const encryptedPassword = SHA256(password).toString();
+
+    const user = await User.findOne({ id, password: encryptedPassword });
+
+    if (!user) {
+      res
+        .status(401)
+        .send({ errorMessage: "아이디 또는 비밀번호를 다시 확인해주세요." });
+      return;
+    }
+
+    const token = jwt.sign({ nickname: user.nickname }, SECRET_KEY);
+
+    res.send({ token });
   } catch (e) {
     console.log(e);
     return res.json({
       ok: false,
-      error: "알수없는 오류가 발생했습니다 관리자에게 분의하세요.",
+      error: "알수없는 오류가 발생했습니다 관리자에게 문의하세요.",
     });
   }
 };
 
-export const join = (req, res) => {
+export const join = async (req, res) => {
   try {
+    const { phoneNumber, id, password, nickname } = req.body;
+    const encryptedPassword = SHA256(password).toString();
+
+    await User.create({
+      phoneNumber,
+      id,
+      password: encryptedPassword,
+      nickname,
+    });
+
+    res.send({});
   } catch (e) {
     console.log(e);
     return res.json({
       ok: false,
-      error: "알수없는 오류가 발생했습니다 관리자에게 분의하세요.",
+      error: "알수없는 오류가 발생했습니다 관리자에게 문의하세요.",
     });
   }
 };
