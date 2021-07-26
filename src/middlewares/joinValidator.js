07f3201f0ddeb84da56fdaddd05d9d37bd8dea9f
coffeed-cat <gwcattower@gmail.com>
2021-07-26 19:58:46 +0900

    [ middlewares/joinValidator.js ] 회원가입 검증 미들웨어 1차 완성

--- src/middlewares/joinValidator.js
+++ src/middlewares/joinValidator.js
@@ -1,8 +1,7 @@
-const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const Joi = require("joi");
 
-const registerValidation = Joi.object({
+const joinValidation = Joi.object({
   phoneNumber: Joi.string().required(),
   id: Joi.string().email().required(),
   password: Joi.string().min(4).trim().required(), //최소 4자이상, 공백제거하고 받음
@@ -13,45 +12,40 @@ const registerValidation = Joi.object({
 //회원가입 검증
 module.exports = async (req, res, next) => {
   try {
-    const { nickname, email, password, confirmPassword } =
-      await registerValidation.validateAsync(req.body);
+    const { phoneNumber, id, password, nickname } =
+      await joinValidation.validateAsync(req.body);
 
     if (password.includes(nickname)) {
       res.status(400).send({
-        errorMessage: "비밀번호는 닉네임이 포함되지 않도록 설정해주세요",
+        errorMessage: "비밀번호에 닉네임이 포함되지 않도록 설정해주세요.",
       });
       return;
     }
 
-    if (email === password) {
-      res.status(400).send({
-        errorMessage: "이메일과 비밀번호는 동일하게 설정할 수 없습니다",
-      });
-    }
     // 닉네임 중복검사
-    const existNick = await User.findOne({ nickname });
+    const isThisNicknameExist = await User.findOne({ nickname });
 
-    if (existNick) {
+    if (isThisNicknameExist) {
       res.status(400).send({
-        errorMessage: "닉네임이 중복되었습니다",
+        errorMessage: "이미 존재하는 닉네임입니다.",
       });
       return;
     }
 
-    // 이메일 중복검사
-    const existemail = await User.findOne({ email });
+    // 아이디 중복검사
+    const isThisIdExist = await User.findOne({ id });
 
-    if (existemail) {
+    if (isThisIdExist) {
       res.status(400).send({
-        errorMessage: "이메일이 중복되었습니다",
+        errorMessage: "이미 존재하는 아이디입니다.",
       });
       return;
     }
 
     next();
   } catch (err) {
-    res.status(401).send({
-      errorMessage: "요청한 형식이 올바르지 않습니다",
+    res.status(400).send({
+      errorMessage: "요청한 형식이 올바르지 않습니다.",
     });
     return;
   }
