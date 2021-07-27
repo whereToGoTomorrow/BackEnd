5b1e9a8ac40a2f694c64f13c5ccbaf498eee5094
정훈박 <fireking5997@gmail.com>
2021-07-27 21:49:25 +0900

    [잘못푸시됨 다시 해야됨]

--- src/app.js
+++ src/app.js
@@ -6,11 +6,8 @@ import "./getDistance";
 import express from "express";
 import globalRouter from "./Routers/globalRouter";
 import detailRouter from "./Routers/detailRouter";
-<<<<<<< HEAD
 import oAuthRouter from "./Routers/oAuthRouter";
-=======
 import authRouter from "./Routers/authRouter";
->>>>>>> fed21e79f233890f736f6c355c897bc5e7df9b7f
 
 const app = express();
 
@@ -21,10 +18,6 @@ const PORT = process.env.PORT || 4000;
 
 app.use("/", globalRouter);
 app.use("/detail", detailRouter);
-<<<<<<< HEAD
 app.use("/oAuth", oAuthRouter);
-
-=======
 app.use("/auth", authRouter);
->>>>>>> fed21e79f233890f736f6c355c897bc5e7df9b7f
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
